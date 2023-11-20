ee873fb368cdda46f593ce161f6174e428038752
Pushkar S <spushkar0071234@gmail.com>
2023-11-20 14:17:55 +0530

    Structs

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct driver {
+    char name[20];
+    char car[20];
+    int available;
+    int latitude;
+    int longitude;
+} driver;
+
+typedef struct passenger {
+    char name[20];
+    int latitude;
+    int longitude;
+} passenger;
+
+int main() {
+    while(1) {
+
+    }
+    return 0;
+}
