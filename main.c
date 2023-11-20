5a257dac3168dbecc8b356aa4cacc7f1af7dc95d
rahul <rahul.baradol.14@gmail.com>
2023-11-20 15:57:33 +0530

    comedy

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct driver {
     char name[20];
@@ -79,6 +80,11 @@ void addDriver(driver d) {
     node -> curDr = d;
     node -> next = NULL;
 
+    if (driverList == NULL) {
+        driverList = node;
+        return;
+    }
+
     allDrivers *tail = driverList;
     while (tail != NULL && tail -> next != NULL) {
         tail = tail -> next;
@@ -92,6 +98,11 @@ void addPassenger(passenger p) {
     node -> curDr = p;
     node -> next = NULL;
 
+    if (passList == NULL) {
+        passList = node;
+        return;
+    }
+
     allPassengers *tail = passList;
     while (tail != NULL && tail -> next != NULL) {
         tail = tail -> next;
@@ -100,25 +111,36 @@ void addPassenger(passenger p) {
     tail -> next = node;
 }
 
+float generateBill(int curLati, int curLongi, int destLati, int destLongi) {
+    float base = 50.12421;
+    float bill = base + sqrt((curLati - destLati) * (curLati - destLati) + (curLongi - destLongi) * (curLongi - destLongi)) * 10;
+    return bill;
+}
+
 int main() {
     int flag = 1;
     while(flag) {
         printf("Are you a driver or a passenger?\n");
         printf("Press 1 for driver.\n");
         printf("Press 2 for passenger.\n");
-        printf("Press q to quit.\n");
+        printf("Press any other number to quit.\n");
         
-        char isDriver;
-        scanf("%c", isDriver);
+        int isDriver;
+        scanf("%d\n", &isDriver);
+
+        if (isDriver != 1 && isDriver != 2) {
+            printf("Shutting down the service...\n");
+            break;
+        }
 
         switch(isDriver) {
-            case '1':
+            case 1:
                 driver curD;
                 printf("Enter your name (less than 20 characters): ");
-                scanf("%s", curD.name);
+                scanf("%s", &curD.name[0]);
 
                 printf("Enter your car number: ");
-                scanf("%s", curD.car);
+                scanf("%s", &curD.car[0]);
 
                 allDrivers *t = driverExists(curD);
                 if (t != NULL) {
@@ -129,22 +151,52 @@ int main() {
                     }
 
                     // Check for bill or something
+                    printf("Type 1 to confirm ride complete.\n");
+                    int endRide = 0;
+                    scanf("%d", &endRide); 
 
+                    if (endRide != 1) {
+                        printf("Continue to ride...\n");
+                        break;
+                    } 
+
+                    int destLatitude, destLongitude;
+
+                    printf("Ending ride...\n");
+                    printf("Enter your current latitude: ");
+                    scanf("%d", &destLatitude);
+
+                    printf("Enter your current longitude: ");
+                    scanf("%d", &destLongitude);
+
+                    printf("Generating bill...\n");
+                    printf("From: \n");
+                    printf("Latitude: %d\n", (t -> curDr).latitude);
+                    printf("Longitude: %d\n", (t -> curDr).longitude);
+                    printf("To: \n");
+                    printf("Latitude: %d\n", destLatitude);
+                    printf("Longitude: %d\n", destLongitude);
+
+                    float bill = generateBill((t -> curDr).latitude, (t -> curDr).longitude, destLatitude, destLongitude);
+                    printf("Bill is %f\n", bill);
+
+                    printf("You are now available for new rides.\n");
+                    (t -> curDr).isAvailable = 1;
                 } else {
                     addDriver(curD);
                 }
                 break;
 
-            case '2':
+            case 2:
                 passenger curP;
                 printf("Enter your name: ");
-                scanf("%s", curP.name);
+                scanf("%s", &curP.name[0]);
 
                 printf("Enter your latitude: ");
-                scanf("%d", curP.latitude);
+                scanf("%d", &curP.latitude);
 
                 printf("Enter your longitude: ");
-                scanf("%d", curP.longitude);
+                scanf("%d", &curP.longitude);
 
                 // Check if the passenger exists
                 int exist = passExists(curP);
@@ -162,13 +214,9 @@ int main() {
                 }
                 break;
 
-            case 'q':
-                printf("Sayonara!!\n");
-                flag = 0;
-                break;
-
             default: 
-                printf("Invalid input.\n");
+                printf("Shutting down the service...\n");
+                flag = 0;
                 break;
         }
     }
