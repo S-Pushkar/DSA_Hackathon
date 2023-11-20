16ced154a82feef6e45740167ff8679b0b21d502
rahul <rahul.baradol.14@gmail.com>
2023-11-20 14:58:23 +0530

    added the driver input part

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct driver {
     char name[20];
     char car[20];
-    int available;
+    int isAvailable;
     int latitude;
     int longitude;
 } driver;
@@ -24,12 +26,71 @@ typedef struct allPassengers {
     struct allPassengers* next;
 } allPassengers;
 
+allDrivers *driverList = NULL;
+allPassengers *passList = NULL;
+
+allDrivers* driverExists(driver d) {
+    allDrivers *tmp = driverList;
+
+    while (tmp != NULL) {
+        int cmp = strcmp((tmp -> curr).car, d.car);
+        if (cmp == 0) {
+            return tmp;
+        }
+
+        tmp = tmp -> next;
+    }
+    
+    return NULL;
+}
+
 int main() {
-    while(1) {
+    int flag = 1;
+    while(flag) {
         printf("Are you a driver or a passenger?\n");
         printf("Press 1 for driver.\n");
         printf("Press 2 for passenger.\n");
-        int 
+        printf("Press q to quit.\n");
+        
+        char isDriver;
+        scanf("%c", isDriver);
+
+        switch(isDriver) {
+            case '1':
+                driver cur;
+                printf("Enter your name (less than 20 characters): ");
+                scanf("%s", cur.name);
+
+                printf("Enter your car number: ");
+                scanf("%s", cur.car);
+
+                allDrivers *t = driverExists(cur);
+                if (t != NULL) {
+                    int isAvail = (t -> curr).isAvailable;
+                    if (isAvail) {
+                        printf("We are looking for passengers. Hold on!\n");
+                        break;
+                    }
+
+                    // Check for bill or something
+                } else {
+                    addPerson();
+                }
+                break;
+
+            case '2':
+
+                break;
+
+            case 'q':
+                printf("Sayonara!!\n");
+                flag = 0;
+                break;
+
+            default: 
+                printf("Invalid input.\n");
+                break;
+        }
     }
     return 0;
 }
